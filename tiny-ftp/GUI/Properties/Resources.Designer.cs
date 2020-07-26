d30a3b5141f9b8fa4ce0c0f90af281214340988f
WTBantoe <928871624@qq.com>
2020-07-26 18:21:52 +0800

    fix: Change project structure from Winform to WPF

--- tiny-ftp/GUI/Properties/Resources.Designer.cs
+++ tiny-ftp/GUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace tiny_ftp.Properties
+namespace GUI.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace tiny_ftp.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("tiny_ftp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- tiny-ftp/GUI/Properties/Settings.Designer.cs
+++ tiny-ftp/GUI/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace tiny_ftp.Properties
+namespace GUI.Properties
 {
 
 
