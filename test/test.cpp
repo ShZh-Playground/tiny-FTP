3a4643eba5c3706bd142c071d9fe65b84f3b1860
Sh-Zh-7 <2431297348@qq.com>
2020-07-27 11:36:31 +0800

    feat: add dll test project.
    Only test login local server at first.

---
+++ test/test.cpp
@@ -0,0 +1,13 @@
+﻿// test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+
+#include "pch.h"
+#include "../client/client.h"
+#include <iostream>
+
+#pragma comment(lib, "client.lib")
+
+int main()
+{
+	IClient* client = GetClient("192.168.176.1");
+  std::cout << client->receive_message() << std::endl;
+}
