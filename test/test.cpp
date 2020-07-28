3052a17c6f885a34dc73c199263eba6c3455718d
Sh-Zh-7 <2431297348@qq.com>
2020-07-28 13:26:04 +0800

    feat: adjust test code to be like unit test

--- test/test.cpp
+++ test/test.cpp
@@ -1,33 +1,50 @@
 ﻿// test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-
 #include "pch.h"
-#include "../client/client/client.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "../client/client/client.h"
+
 
 #pragma comment(lib, "client.lib")
 
 using namespace std;
 
-int main()
-{
-	// 初始化
+IClient* Initialize() {
   iostream::sync_with_stdio(false);
   IClient* client = GetClient("192.168.176.1");
   client->Login("shzh", "123456");
+	return client;
+}
 
-	// client->DownloadFile("zj.m");
-	client->UploadFile("image.jpg");
+void TestDownload(const std::string& filename) {
+	auto iclient = Initialize();
+	iclient->DownloadFile(filename);
+	delete iclient;
+}
 
-	// // 列出所有目录
-	//auto file_names = client->GetDirList();
-	//for (auto file : file_names) {
-	//	cout << file << endl;
-	//}
+void TestUpload(const std::string& filename) {
+  auto iclient = Initialize();
+	iclient->UploadFile(filename);
+  delete iclient;
+}
 
-	// // 获取服务器文件的大小
-	// cout << "FileSize: " << client->GetFileSize("zj.m") << endl;
+void TestListDir() {
+  auto iclient = Initialize();
+  auto file_names = iclient->GetDirList();
+  for (auto file : file_names) {
+  	cout << file << endl;
+  }
+  delete iclient;
+}
 
-	// 结束
-	delete client;
-	return 0;
+void TestGetFileSize(const std::string& filename) {
+  auto iclient = Initialize();
+  cout << "FileSize: " << iclient->GetFileSize("zj.m") << endl << endl;
+  delete iclient;
+}
+
+int main() {
+	TestDownload("image.jpg");
+  return 0;
 }
